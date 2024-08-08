a10e1f328e33d930e33530b0df4e1c42fb5a281f
JB <163941305+janders1701@users.noreply.github.com>
2024-08-07 21:31:24 -0500

    updates

--- 01_Hello_World_Again/unsolved/js/index.js
+++ 01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,5 @@
 // Create a variable below
-
+var helloWorld = "Hello World!"
+var myFirstVar = "varValue2"
 // Console.log() your variable here.
+console.log (helloWorld);
--- 02_More_Variables/unsolved/js/index.js
+++ 02_More_Variables/unsolved/js/index.js
@@ -1,3 +1,38 @@
 // Create the addNumbers function here.
+var x = 20;
+var y = 10;
+var sum = x + y;
+console.log (sum)
 
 // Create the stringConcat function here.
+var a = "blah"
+console.log(a)
+
+var adj = "annoying";
+var noun = "cat";
+var madlib = "the " + adj + " " + noun + "jumps on your face"
+console.log(madlib)
+
+
+function addNumbers () {
+    var two = 2;
+    var three = 3;
+
+    var five = two + three;
+
+    console.log(five)
+}
+
+addNumbers();
+
+function stringConcat() {
+    var pokemon = "eevee";
+    var pokeball = "pokeball";
+
+    var pokemaster = pokemon + pokeball;
+
+    alert(pokemaster);
+
+}
+
+stringConcat();
